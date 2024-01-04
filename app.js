1e8280429bb9961566c38dd2b76f95683b19fa4e
Fatih Ataç <fatihatac14@gmail.com>
2024-01-04 14:51:01 +0300

    for cv a github page url added

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ $("#page-top").on("click", function () {
   $(".navbar-collapse").collapse("hide");
 });
 
-// $("#download-button").on("click", function () {
-//   window.location.href = "./assets/Fatih Ataç CV.pdf";
-// });
+
+
 
