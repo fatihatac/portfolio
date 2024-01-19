8c2282614eb541152bb90e1aecae622793c6952b
Fatih Ataç <fatihatac14@gmail.com>
2024-01-19 15:27:14 +0300

    Added new project

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ $("#page-top").on("click", function () {
   $(".navbar-collapse").collapse("hide");
 });
 
+$("#projects a").each((index, e) => console.log(e.target));
 
-// console.log();
-
-$("#projects a").each((index,e) => console.log(e.target));
+document.querySelector("img").classList.toggle;
